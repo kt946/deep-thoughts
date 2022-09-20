cf2262ced0ef9181d9b7d3eeb69d48ca00f99204
KT 2022 <103476893+kt946@users.noreply.github.com>
2022-09-19 20:29:45 -0400

    added mutations for thoughts, friends, and reactions

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
-//import ApolloServer
+// import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
+// import middleware4
+const { authMiddleware } = require('./utils/auth');
 
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
@@ -10,7 +12,8 @@ const PORT = process.env.PORT || 3001;
 // create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 });
 
 const app = express();
